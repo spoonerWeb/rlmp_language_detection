753fd8881fe70c6c7f61c15b9032c939af9c3392
Frank <frankmoll@gmx.net>
2016-02-24 12:14:45 +0100

    [BUG] Locallang was not loaded, mistake in path

--- Configuration/TCA/Overrides/sys_domain.php
+++ Configuration/TCA/Overrides/sys_domain.php
@@ -3,7 +3,7 @@ defined('TYPO3_MODE') or die();
 
 use \TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
-$ll_path = 'LLL:EXT:rlmp_language_detection/Resources/Private/Language/locallang_db.xlf:';
+$ll_path = 'LLL:EXT:rlmp_language_detection/Resources/Private/Language/locallang_db.xlf';
 $temporaryColumns  = array (
                 				'tx_rlmplanguagedetection_disabledetection' => array (
                 					'exclude' => 1,
