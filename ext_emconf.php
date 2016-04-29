b9202b99ee5e3eace265e01119493931c551483f
Thomas Löffler <loeffler@spooner-web.de>
2016-04-29 13:26:54 +0200

    [TASK] Opens PHP version on the upper side to support PHP 7

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,7 +26,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.3.0-5.6.99',
+			'php' => '5.3.0',
 			'typo3' => '6.2.0-7.99.99',
 			'static_info_tables' => ''
 		),
