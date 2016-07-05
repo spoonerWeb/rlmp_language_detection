7f990c13c6d591a0c0ccf111f23fcf45bfab5b46
Thomas Löffler <loeffler@spooner-web.de>
2016-07-05 22:32:07 +0200

    [TASK] Removes upper dependency. Works on 8.2.

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.0',
-			'typo3' => '6.2.0-7.99.99',
+			'typo3' => '6.2.0',
 			'static_info_tables' => '6.3.0'
 		),
 		'conflicts' => array(
